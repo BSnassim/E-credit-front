bbdb5e711759343480e379418d0eaea1b6ddedc7
AKM-FR <khalil-frikha@outlook.fr>
2022-04-07 14:18:17 +0000

    22nd commit

--- src/app/main/app-inline-menu/app.inlinemenu.component.ts
+++ src/app/main/app-inline-menu/app.inlinemenu.component.ts
@@ -1,42 +1,67 @@
-import { User } from 'src/app/models/user';
-import { TokenService } from './../../auth/services/token.service';
-import { UserService } from 'src/app/Services/user.service';
-import { Component, OnInit } from '@angular/core';
-import { AppMainComponent } from '../app-main/app.main.component';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { User } from "src/app/models/user";
+import { TokenService } from "./../../auth/services/token.service";
+import { UserService } from "src/app/Services/user.service";
+import { Component, OnInit } from "@angular/core";
+import { AppMainComponent } from "../app-main/app.main.component";
+import {
+    trigger,
+    state,
+    style,
+    transition,
+    animate,
+} from "@angular/animations";
 import { AuthService } from "../../auth/services/auth.service";
 
 @Component({
-    selector: 'app-inlinemenu',
-    templateUrl: './app.inlinemenu.component.html',
+    selector: "app-inlinemenu",
+    templateUrl: "./app.inlinemenu.component.html",
     animations: [
-        trigger('inline', [
-            state('hidden', style({
-                height: '0px',
-                overflow: 'hidden'
-            })),
-            state('visible', style({
-                height: '*',
-            })),
-            state('hiddenAnimated', style({
-                height: '0px',
-                overflow: 'hidden'
-            })),
-            state('visibleAnimated', style({
-                height: '*',
-            })),
-            transition('visibleAnimated => hiddenAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)')),
-            transition('hiddenAnimated => visibleAnimated', animate('400ms cubic-bezier(0.86, 0, 0.07, 1)'))
-        ])
-    ]
+        trigger("inline", [
+            state(
+                "hidden",
+                style({
+                    height: "0px",
+                    overflow: "hidden",
+                })
+            ),
+            state(
+                "visible",
+                style({
+                    height: "*",
+                })
+            ),
+            state(
+                "hiddenAnimated",
+                style({
+                    height: "0px",
+                    overflow: "hidden",
+                })
+            ),
+            state(
+                "visibleAnimated",
+                style({
+                    height: "*",
+                })
+            ),
+            transition(
+                "visibleAnimated => hiddenAnimated",
+                animate("400ms cubic-bezier(0.86, 0, 0.07, 1)")
+            ),
+            transition(
+                "hiddenAnimated => visibleAnimated",
+                animate("400ms cubic-bezier(0.86, 0, 0.07, 1)")
+            ),
+        ]),
+    ],
 })
 export class AppInlineMenuComponent implements OnInit {
+    currentUser: User = new User();
 
-    currentUser : User = new User;
-
-    constructor(public appMain: AppMainComponent,
+    constructor(
+        public appMain: AppMainComponent,
         private authService: AuthService,
-        private tokenService: TokenService) { }
+        private tokenService: TokenService
+    ) {}
 
     ngOnInit(): void {
         this.loadUser();
@@ -47,7 +72,7 @@ export class AppInlineMenuComponent implements OnInit {
     }
 
     loadUser() {
-        this.tokenService.getUser().subscribe( data=>{
+        this.tokenService.getUser().subscribe((data) => {
             this.currentUser = data;
         });
     }
