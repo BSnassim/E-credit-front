5168ec8e3d7c17ef3e426ffb8b953327812092ac
AKM-FR <khalil-frikha@outlook.fr>
2022-04-14 16:00:12 +0000

    35th commit

--- src/app/Client/credit-details/credit-details.component.ts
+++ src/app/Client/credit-details/credit-details.component.ts
@@ -1,52 +1,47 @@
-import { Garantie } from 'src/app/models/credit/garantie';
-import { CreditFormService } from 'src/app/Services/credit-form-service.service';
-import { Demande } from 'src/app/models/credit/info-personnel';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AppBreadcrumbService } from 'src/app/main/app-breadcrumb/app.breadcrumb.service';
+import { Garantie } from "src/app/models/credit/garantie";
+import { CreditFormService } from "src/app/Services/credit-form-service.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { AppBreadcrumbService } from "src/app/main/app-breadcrumb/app.breadcrumb.service";
+import { Demande } from "src/app/models/credit/demande";
 
 @Component({
-  selector: 'app-credit-details',
-  templateUrl: './credit-details.component.html',
-  styleUrls: ['./credit-details.component.scss']
+    selector: "app-credit-details",
+    templateUrl: "./credit-details.component.html",
+    styleUrls: ["./credit-details.component.scss"],
 })
 export class CreditDetailsComponent implements OnInit {
+    demande = {} as Demande;
 
-  demande = {} as Demande;
+    garanties = [] as any[];
 
-  garanties = [] as any[];
+    constructor(
+        private breadcrumbService: AppBreadcrumbService,
+        private route: ActivatedRoute,
+        private demandeService: CreditFormService
+    ) {
+        this.breadcrumbService.setItems([
+            { label: "Liste des credits", routerLink: ["credit/consultation"] },
+            { label: "Details du demande" },
+        ]);
+    }
 
-  constructor(
-    private breadcrumbService: AppBreadcrumbService,
-    private route: ActivatedRoute,
-    private demandeService: CreditFormService) {
-    this.breadcrumbService.setItems([
-      { label: "Liste des credits", routerLink: ['credit/consultation']},
-      { label: "Details du demande" }
-    ]);
-  }
+    ngOnInit(): void {
+        this.route.params.subscribe((params) => {
+            this.getDemande(params.id);
+            this.getGaranties(params.id);
+        });
+    }
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.getDemande(params.id);
-      this.getGaranties(params.id);
-    })
-  }
+    getGaranties(id: number) {
+        this.demandeService.getGarantiesByDemande(id).subscribe((data) => {
+            this.garanties = data;
+        });
+    }
 
-  getGaranties(id: number){
-    this.demandeService.getGarantiesByDemande(id).subscribe(
-      data =>{
-        this.garanties = data;
-      }
-    );
-  }
-
-  getDemande(id: number) {
-    this.demandeService.getDemandeById(id).subscribe(
-      data =>{
-        this.demande = data;
-      }
-    );
-  }
-
+    getDemande(id: number) {
+        this.demandeService.getDemandeById(id).subscribe((data) => {
+            this.demande = data;
+        });
+    }
 }
