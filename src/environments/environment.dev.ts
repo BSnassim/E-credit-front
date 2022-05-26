85a23a08975370ae2484b02673ed344f3f5db2fb
AKM-FR <khalil-frikha@outlook.fr>
2022-05-26 15:05:29 +0000

    updating client's dashboard

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     title: "DEV",
-    apiURL: "http://192.168.107.112:8088",
-    adminURL: "http://192.168.107.112:8088/administration",
+    apiURL: "http://192.168.1.18:8088",
+    adminURL: "http://192.168.1.18:8088/administration",
 };
