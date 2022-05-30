1d18fae7566ef880d40d6cc541628ffcd125d0f0
AKM-FR <khalil-frikha@outlook.fr>
2022-05-30 17:42:51 +0000

    modifier charge dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,12 @@
-import { NgxPermissionsService } from "ngx-permissions";
-import { NgxRolesService } from "ngx-permissions";
 import { Component, OnInit } from "@angular/core";
 import { AppBreadcrumbService } from "../main/app-breadcrumb/app.breadcrumb.service";
-import { DemandeRdv } from "../models/demande-rdv";
 import { User } from "../models/user";
 
 import { TokenService } from "../auth/services/token.service";
 
 import { CreditFormService } from "../Services/credit-form-service.service";
 import { Historique } from "../models/historique";
+import { Demande } from "../models/credit/demande";
 
 @Component({
     selector: "app-dashboard",
@@ -16,6 +14,48 @@ import { Historique } from "../models/historique";
     styleUrls: ["./dashboard.component.scss"],
 })
 export class DashboardComponent implements OnInit {
+    barData: any;
+
+    pieData: any;
+
+    barOptions: any;
+
+    pieOptions: any;
+
+    accepted: number = 0;
+
+    rejected: number = 0;
+
+    modified: number = 0;
+
+    submitted: number = 0;
+
+    returned: number = 0;
+
+    demandes: Demande[] = [];
+
+    JanDemands: Demande[] = [];
+
+    FebDemands: Demande[] = [];
+
+    MarDemands: Demande[] = [];
+
+    AprDemands: Demande[] = [];
+
+    MayDemands: Demande[] = [];
+
+    JuiDemands: Demande[] = [];
+
+    JulDemands: Demande[] = [];
+
+    AugDemands: Demande[] = [];
+
+    SepDemands: Demande[] = [];
+
+    OctDemands: Demande[] = [];
+
+    NovDemands: Demande[] = [];
+
     historiques: Historique[] = [];
 
     currentUser: User = new User();
@@ -33,71 +73,140 @@ export class DashboardComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.loadUser();
-        // console.log(this.LoadHistoriqueDemande("12345678"));
-        // this.getLibEtapePhase().then((res) => {
-        //     this.customEvents = [
-        //         {
-        //             // idPhase=1
-        //             status: res.find((i) => i.id == 1).etape,
-        //             date: res.find((i) => i.id == 1).enAttenteDe,
-        //             icon: PrimeIcons.DOWNLOAD,
-        //             color: "rgba(228, 0, 123, 1)",
-        //         },
-        //         {
-        //             // idPhase=1
-        //             status: "Demande en cours de traitement",
-        //             // date: "15/10/2020 14:00",
-        //             icon: PrimeIcons.COG,
-        //             color: "rgba(255, 170, 0, 1)",
-        //         },
-        //         {
-        //             // idPhase=4
-        //             status: res.find((i) => i.id == 4).etape,
-        //             date: res.find((i) => i.id == 4).enAttenteDe,
-        //             icon: PrimeIcons.PLUS,
-        //             color: "rgba(186, 0, 255, 1)",
-        //         },
-        //         {
-        //             // idPhase=5
-        //             status: res.find((i) => i.id == 5).etape,
-        //             date: res.find((i) => i.id == 5).enAttenteDe,
-        //             icon: PrimeIcons.PENCIL,
-        //             color: "rgba(0, 186, 255, 1)",
-        //         },
-        //         {
-        //             // idPhase=3
-        //             status: res.find((i) => i.id == 3).etape,
-        //             date: "Pour plus d'informations contacter le responsable de votre agence",
-        //             icon: PrimeIcons.TIMES,
-        //             color: "rgba(255, 0, 0, 1)",
-        //         },
-        //         {
-        //             // idPhase=2
-        //             status: res.find((i) => i.id == 2).etape,
-        //             // info: "Rendez-vous dans votre agence le :",
-        //             date: res.find((i) => i.id == 2).enAttenteDe,
-        //             icon: PrimeIcons.CHECK,
-        //             color: "rgba(2, 217, 7, 0.82)",
-        //         },
-        //     ];
-        // });
+        this.loadUserInfo();
     }
 
-    LoadHistoriqueDemande(id: string) {
+    loadHistoriqueDemande(id: string) {
         this.creditService.getHistoriqueDemandeRecente(id).subscribe((data) => {
-            setTimeout(() => {
-                this.historiques = data;
-                console.log(this.historique);
-            }, 1000);
+            this.historiques = data;
         });
     }
 
-    loadUser() {
+    loadUserInfo() {
         this.tokenService.getUser().subscribe((data) => {
             this.currentUser = data;
-
-            this.LoadHistoriqueDemande(data.id);
+            this.loadHistoriqueDemande(data.id);
+            this.loadDemandsForManager(data.agence.idAgence);
         });
+    }
+
+    loadDemandsForManager(id: number) {
+        this.creditService
+            .getAllDemandesByAgenceAndByYear(id)
+            .subscribe((data) => {
+                this.demandes = data;
+                this.demandes.forEach((demande) => {
+                    if (demande.idPhase === 1) {
+                        this.submitted++;
+                    }
+                    if (demande.idPhase === 2) {
+                        this.accepted++;
+                    }
+                    if (demande.idPhase === 3) {
+                        this.rejected++;
+                    }
+                    if (demande.idPhase === 4) {
+                        this.returned++;
+                    }
+                    if (demande.idPhase === 5) {
+                        this.modified++;
+                    }
+                });
+
+                // Object.keys(this.demandes).length  for  table length
+
+                this.barData = {
+                    labels: [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                    ],
+                    datasets: [
+                        {
+                            label: "Les Demandes rejetés",
+                            backgroundColor: "rgb(255, 99, 132)",
+                            borderColor: "rgb(255, 99, 132)",
+                            data: [65, 59, 80, 81, 56, 55, 40, 50, 50, 22, 33],
+                        },
+                        {
+                            label: "Les demandes accordés",
+                            backgroundColor: "rgb(2, 217, 7)",
+                            borderColor: "rgb(2, 217, 7)",
+                            data: [28, 48, 40, 19, 86, 27, 90, 11, 42, 32, 33],
+                        },
+                    ],
+                };
+                this.barOptions = {
+                    plugins: {
+                        legend: {
+                            labels: {
+                                fontColor: "#A0A7B5",
+                            },
+                        },
+                    },
+                    scales: {
+                        x: {
+                            ticks: {
+                                color: "#A0A7B5",
+                            },
+                            grid: {
+                                color: "rgba(160, 167, 181, .3)",
+                            },
+                        },
+                        y: {
+                            ticks: {
+                                color: "#A0A7B5",
+                            },
+                            grid: {
+                                color: "rgba(160, 167, 181, .3)",
+                            },
+                        },
+                    },
+                };
+                this.pieData = {
+                    labels: [
+                        "Les Demandes Soumises",
+                        "Les demandes accordés",
+                        "Les Demandes rejetés",
+                        "En Attente d'un Complément",
+                        "Les Demandes Modifiées",
+                    ],
+                    datasets: [
+                        {
+                            data: [
+                                this.submitted,
+                                this.accepted,
+                                this.rejected,
+                                this.returned,
+                                this.modified,
+                            ],
+                            backgroundColor: [
+                                "rgb(255, 205, 86)",
+                                "rgb(2, 217, 7)",
+                                "rgb(255, 99, 132)",
+                                "rgb(186, 0, 255)",
+                                "rgb(75, 192, 192)",
+                            ],
+                        },
+                    ],
+                };
+                this.pieOptions = {
+                    plugins: {
+                        legend: {
+                            labels: {
+                                fontColor: "#A0A7B5",
+                            },
+                        },
+                    },
+                };
+            });
     }
 }
--- src/app/main/app-breadcrumb/app.breadcrumb.component.ts
+++ src/app/main/app-breadcrumb/app.breadcrumb.component.ts
@@ -1,34 +1,50 @@
-import { Component, OnDestroy } from '@angular/core';
-import { AppBreadcrumbService } from './app.breadcrumb.service';
-import { Subscription } from 'rxjs';
-import { MenuItem } from 'primeng/api';
-import {AppMainComponent} from '../app-main/app.main.component';
+import { Component, OnDestroy } from "@angular/core";
+import { AppBreadcrumbService } from "./app.breadcrumb.service";
+import { Subscription } from "rxjs";
+import { MenuItem } from "primeng/api";
+import { AppMainComponent } from "../app-main/app.main.component";
+import { EventsService } from "src/app/Services/events.service";
+import { DemandeRdv } from "src/app/models/demande-rdv";
 
 @Component({
-    selector: 'app-breadcrumb',
-    templateUrl: './app.breadcrumb.component.html'
+    selector: "app-breadcrumb",
+    templateUrl: "./app.breadcrumb.component.html",
 })
 export class AppBreadcrumbComponent implements OnDestroy {
-
     subscription: Subscription;
 
     items: MenuItem[];
 
     home: MenuItem;
 
+    events: DemandeRdv[] = [];
+
     search: string;
 
-    constructor(public breadcrumbService: AppBreadcrumbService, public appMain: AppMainComponent) {
-        this.subscription = breadcrumbService.itemsHandler.subscribe(response => {
-            this.items = response;
-        });
+    constructor(
+        public breadcrumbService: AppBreadcrumbService,
+        public appMain: AppMainComponent,
+        private eventsService: EventsService
+    ) {
+        this.subscription = breadcrumbService.itemsHandler.subscribe(
+            (response) => {
+                this.items = response;
+            }
+        );
 
-        this.home = { icon: 'pi pi-home', routerLink: '/' };
+        this.home = { icon: "pi pi-home", routerLink: "/" };
     }
 
     ngOnDestroy() {
         if (this.subscription) {
             this.subscription.unsubscribe();
         }
+    }
+
+    getEvent(id: string) {
+        this.eventsService.getRdvByIdUserAPI(id).subscribe((data) => {
+            this.events = data;
+            return data;
+        });
     }
 }
