e9e7044e331a72778379f459bd004345229deeb3
BSnassim <72417267+BSnassim@users.noreply.github.com>
2022-05-31 15:04:26 +0100

    Update app.breadcrumb.component.ts

--- src/app/main/app-breadcrumb/app.breadcrumb.component.ts
+++ src/app/main/app-breadcrumb/app.breadcrumb.component.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import { Component, OnDestroy } from "@angular/core";
-import { AppBreadcrumbService } from "./app.breadcrumb.service";
-import { Subscription } from "rxjs";
-import { MenuItem } from "primeng/api";
-import { AppMainComponent } from "../app-main/app.main.component";
-import { EventsService } from "src/app/Services/events.service";
-import { DemandeRdv } from "src/app/models/demande-rdv";
-=======
 import { Component, OnDestroy } from '@angular/core';
 import { AppBreadcrumbService } from './app.breadcrumb.service';
 import { Subscription } from 'rxjs';
@@ -14,35 +5,21 @@ import { MenuItem } from 'primeng/api';
 import {AppMainComponent} from '../app-main/app.main.component';
 import { TokenService } from 'src/app/auth/services/token.service';
 import { EventsService } from 'src/app/Services/events.service';
->>>>>>> origin/master
 
 @Component({
-    selector: "app-breadcrumb",
-    templateUrl: "./app.breadcrumb.component.html",
+    selector: 'app-breadcrumb',
+    templateUrl: './app.breadcrumb.component.html'
 })
 export class AppBreadcrumbComponent implements OnDestroy {
+
     subscription: Subscription;
 
     items: MenuItem[];
 
     home: MenuItem;
 
-    events: DemandeRdv[] = [];
-
     search: string;
 
-<<<<<<< HEAD
-    constructor(
-        public breadcrumbService: AppBreadcrumbService,
-        public appMain: AppMainComponent,
-        private eventsService: EventsService
-    ) {
-        this.subscription = breadcrumbService.itemsHandler.subscribe(
-            (response) => {
-                this.items = response;
-            }
-        );
-=======
     events: number;
 
     constructor(public breadcrumbService: AppBreadcrumbService, public appMain: AppMainComponent,private tokenService: TokenService,
@@ -50,9 +27,8 @@ export class AppBreadcrumbComponent implements OnDestroy {
         this.subscription = breadcrumbService.itemsHandler.subscribe(response => {
             this.items = response;
         });
->>>>>>> origin/master
 
-        this.home = { icon: "pi pi-home", routerLink: "/" };
+        this.home = { icon: 'pi pi-home', routerLink: '/' };
     }
 
     ngOnInit(): void {
@@ -75,12 +51,5 @@ export class AppBreadcrumbComponent implements OnDestroy {
         if (this.subscription) {
             this.subscription.unsubscribe();
         }
-    }
-
-    getEvent(id: string) {
-        this.eventsService.getRdvByIdUserAPI(id).subscribe((data) => {
-            this.events = data;
-            return data;
-        });
     }
 }
