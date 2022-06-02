b3dffedb6d89b50d22e269c410277e2439e23253
nassim <naseem.bensalah@gmail.com>
2022-06-02 17:14:18 +0100

    changed default mode to light

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ export class AppComponent implements OnInit {
 
     ripple: boolean;
 
-    colorScheme = "dark";
+    colorScheme = "light";
 
     constructor(
         private primengConfig: PrimeNGConfig,
@@ -26,10 +26,51 @@ export class AppComponent implements OnInit {
     ) {}
 
     ngOnInit() {
+        this.changeStyleSheetsColor('layout-css', 'layout-' + 'light' + '.css', 1);
+        this.changeStyleSheetsColor('theme-css', 'theme-' + 'light' + '.css', 1);
+
         this.primengConfig.ripple = true;
         this.ripple = true;
         this.permissionsService.loadPermissions(
             this.authService.getPermissions()
         );
+    }
+
+    changeStyleSheetsColor(id, value, from) {
+        const element = document.getElementById(id);
+        const urlTokens = element.getAttribute('href').split('/');
+
+        if (from === 1) {           // which function invoked this function - change scheme
+            urlTokens[urlTokens.length - 1] = value;
+        } else if (from === 2) {       // which function invoked this function - change color
+            urlTokens[urlTokens.length - 2] = value;
+        }
+
+        const newURL = urlTokens.join('/');
+
+        this.replaceLink(element, newURL);
+    }
+
+    replaceLink(linkElement, href) {
+        if (this.isIE()) {
+            linkElement.setAttribute('href', href);
+        } else {
+            const id = linkElement.getAttribute('id');
+            const cloneLinkElement = linkElement.cloneNode(true);
+
+            cloneLinkElement.setAttribute('href', href);
+            cloneLinkElement.setAttribute('id', id + '-clone');
+
+            linkElement.parentNode.insertBefore(cloneLinkElement, linkElement.nextSibling);
+
+            cloneLinkElement.addEventListener('load', () => {
+                linkElement.remove();
+                cloneLinkElement.setAttribute('id', id);
+            });
+        }
+    }
+
+    isIE() {
+        return /(MSIE|Trident\/|Edge\/)/i.test(window.navigator.userAgent);
     }
 }
